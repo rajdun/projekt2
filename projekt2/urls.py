3c053a2cfba9e32214a13a67e5c7cbcc26235f60
rajdun <71730219+rajdun@users.noreply.github.com>
2023-05-26 20:22:04 +0200

    Update urls.py

--- projekt2/urls.py
+++ projekt2/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
                   path('inventory', include('inventory.urls')),
                   path('', include('orders.urls')),
                   path('', include('cart.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
